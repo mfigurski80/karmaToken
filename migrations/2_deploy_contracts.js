6bcf5ba3b65c5ee346bbb38ab461dc667a4eab44
Mikolaj Figurski <meek.f80@gmail.com>
2021-08-27 13:14:15 -0400

    Fix migration issue

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
+const CollateralManager = artifacts.require('./CollateralManager.sol');
 const LoanToken = artifacts.require('./LoanToken.sol');
 
-module.exports = function(deployer) {
-  deployer.deploy(LoanToken);
+module.exports = async function(deployer) {
+  await deployer.deploy(CollateralManager);
+  collateralManager = await CollateralManager.deployed();
+  deployer.deploy(LoanToken, collateralManager.address);
 }
