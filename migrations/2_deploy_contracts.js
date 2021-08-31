143704d9970633c24551d5ccab6a3c12149f8b2c
Mikolaj Figurski <meek.f80@gmail.com>
2021-08-31 11:47:30 -0400

    Mark monetized nft for migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 const CollateralManager = artifacts.require('./CollateralManager.sol');
-const LoanToken = artifacts.require('./LoanToken.sol');
+const MonetizedLoanNFT = artifacts.require('./MonetizedLoanNFT.sol');
 
 module.exports = async function(deployer) {
   await deployer.deploy(CollateralManager);
   collateralManager = await CollateralManager.deployed();
-  await deployer.deploy(LoanToken, collateralManager.address);
+  await deployer.deploy(MonetizedLoanNFT, collateralManager.address);
 }
