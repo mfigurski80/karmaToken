5612f82d0fedcbfdc2385b042715d837e2a3e0fa
Mikolaj Figurski <meek.f80@gmail.com>
2021-09-02 19:13:26 -0400

    Replace LoanToken sol test with js

--- test/LoanToken.test.js
+++ test/LoanToken.test.js
@@ -10,6 +10,14 @@ contract('LoanToken', accounts => {
         instance = await LoanToken.new(accounts[accounts.length - 1], { from: ownerAccount });
     });
 
+    it('should implement ERC165 and other interfaces', async () => {
+        const erc165 = await instance.supportsInterface.call('0x01ffc9a7');
+        assert.isTrue(erc165);
+        const erc721 = await instance.supportsInterface.call('0x80ac58cd');
+        assert.isTrue(erc721);
+        const nullInterface = await instance.supportsInterface.call('0xffffffff');
+        assert.isFalse(nullInterface);
+    });
 
     it('should have a symbol and name', async () => {
         const symbol = await instance.symbol();
@@ -19,12 +27,19 @@ contract('LoanToken', accounts => {
     });
 
     it('should allow minting loans', async () => {
-        let tx = await instance.mintLoan(Date.now() - TIME_UNIT.WEEK, TIME_UNIT.DAY, 100);
+        let tx = await instance.mintLoan(Date.now() + TIME_UNIT.WEEK, TIME_UNIT.DAY, 100);
         let ev = await getEvent(tx, 'LoanCreated');
         assert.equal(ev.id, 0);
 
         const owner = await instance.ownerOf.call(ev.id);
         assert.equal(owner, ownerAccount, 'Loan owner was not set to creator by default');
+
+        const balance = await instance.balanceOf.call(ownerAccount);
+        assert.equal(balance.toNumber(), 1, 'Balance did not increase when minting token');
+
+        const loan = await instance.loans.call(ev.id);
+        assert.isTrue(loan.active, 'Loan was not marked as active by default');
+        assert.equal(loan.balance.toNumber(), 100, 'Loan balance was not set to mint parameter');
     });
 
     it('allows loan beneficiary to be changed', async () => {
