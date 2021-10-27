2e650eba13b9eb6c58e34fbc88f1aec46ed0219c
Mikolaj Figurski <meek.f80@gmail.com>
2021-10-27 18:26:05 -0400

    Make BondDataExposed actually expose BondData

--- contracts/exposed/BondDataExposed.sol
+++ contracts/exposed/BondDataExposed.sol
@@ -5,16 +5,14 @@ import "../BondData.sol";
 
 contract BondDataExposed is BondData {
     function writeBondAlpha(uint256 id, bytes32 alp) public {
-        bonds[id * 2] = alp;
+        return _writeBondAlpha(id, alp);
     }
 
     function writeBondBeta(uint256 id, bytes32 bet) public {
-        bonds[id * 2 + 1] = bet;
+        return _writeBondBeta(id, bet);
     }
 
     function addBond(bytes32 alp, bytes32 bet) public returns (uint256 id) {
-        id = bonds.length / 2;
-        bonds.push(alp);
-        bonds.push(bet);
+        return _addBond(alp, bet);
     }
 }
