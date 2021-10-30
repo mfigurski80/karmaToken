83342e415197e80b89cf088c0e144cbb9f97bd91
Mikolaj Figurski <meek.f80@gmail.com>
2021-10-30 18:56:28 -0400

    Add currencyId field to account for ERC1155 references

--- contracts/CurrencyManager.sol
+++ contracts/CurrencyManager.sol
@@ -4,16 +4,35 @@ pragma solidity ^0.8.0;
 struct Currency {
     uint8 currencyType; // {ERC20, ERC721, ERC1155} = {0,1,2}
     address location;
+    uint256 ERC1155Id; // if ERC1155, use extra slot
 }
 
 contract CurrencyManager {
     Currency[] public currencies;
 
-    event CurrencyAdded(uint256 id, uint8 currencyType, address location);
+    event CurrencyAdded(
+        uint256 id,
+        uint8 currencyType,
+        address location,
+        uint256 currencyId
+    );
 
-    function addCurrency(uint8 currencyType, address location) public {
+    constructor() {
+        currencies.push(Currency(0, address(0), 0));
+    }
+
+    function addCurrency(
+        uint8 currencyType,
+        address location,
+        uint256 currencyId
+    ) public {
         require(currencyType < 3, "CurrencyManager: type value not supported");
-        currencies.push(Currency(currencyType, location));
-        emit CurrencyAdded(currencies.length - 1, currencyType, location);
+        currencies.push(Currency(currencyType, location, currencyId));
+        emit CurrencyAdded(
+            currencies.length - 1,
+            currencyType,
+            location,
+            currencyId
+        );
     }
 }
--- test/CurrencyManager.test.js
+++ test/CurrencyManager.test.js
@@ -13,14 +13,14 @@ contract("CurrencyManager", accounts => {
     it('allows adding new currencies', async () => {
         let tx = await instance.addCurrency(0, accounts[0]);
         let ev = await getEvent(tx, 'CurrencyAdded');
-        assert.equal(ev.id.toNumber(), 0);
+        assert.equal(ev.id.toNumber(), 1, "Id is 1, since 0 refers to ether");
         assert.equal(ev.currencyType.toNumber(), 0);
         assert.equal(ev.location, accounts[0]);
     });
 
     it('exposes existing currencies', async () => {
         await instance.addCurrency(0, accounts[0]);
-        let c = await instance.currencies(0);
+        let c = await instance.currencies(1);
         assert.equal(c.currencyType.toNumber(), 0);
         assert.equal(c.location, accounts[0]);
     });
