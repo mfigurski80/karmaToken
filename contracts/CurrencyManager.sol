1ff767bac83215bd1cbaca2a9905febba7b117c5
Mikolaj Figurski <meek.f80@gmail.com>
2021-10-31 18:14:55 -0400

    Restructure CurrencyManager to allow independent type registering

--- contracts/CurrencyManager.sol
+++ contracts/CurrencyManager.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.0;
 
 struct Currency {
-    uint8 currencyType; // {ERC20, ERC721, ERC1155} = {0,1,2}
+    uint8 currencyType; // {ERC20, ERC721, ERC1155 tokens, ERC1155 NFTs} = {0,1,2,3}
+    uint88 ERC1155SmallId;
     address location;
-    uint256 ERC1155Id; // if ERC1155, use extra slot
+    uint256 ERC1155Id; // if ERC1155 and id > 500 septillion, use extra slot
 }
 
 contract CurrencyManager {
@@ -14,25 +15,49 @@ contract CurrencyManager {
         uint256 id,
         uint8 currencyType,
         address location,
-        uint256 currencyId
+        uint256 ERC1155Id
     );
 
     constructor() {
-        currencies.push(Currency(0, address(0), 0));
+        currencies.push(Currency(0, 0, address(0), 0));
     }
 
-    function addCurrency(
-        uint8 currencyType,
-        address location,
-        uint256 currencyId
-    ) public {
-        require(currencyType < 3, "CurrencyManager: type value not supported");
-        currencies.push(Currency(currencyType, location, currencyId));
+    function addERC20Currency(address location) public payable virtual {
+        _addCurrency(Currency(0, 0, location, 0));
+    }
+
+    function addERC721Currency(address location) public payable virtual {
+        _addCurrency(Currency(1, 0, location, 0));
+    }
+
+    function addERC1155TokenCurrency(address location, uint256 tokenId)
+        public
+        payable
+        virtual
+    {
+        Currency memory c = Currency(2, uint88(tokenId), location, 0);
+        if (c.ERC1155SmallId != tokenId) {
+            c.ERC1155Id = tokenId;
+        }
+        _addCurrency(c);
+    }
+
+    function addERC1155Currency(address location) public payable virtual {
+        _addCurrency(Currency(3, 0, location, 0));
+    }
+
+    function _addCurrency(Currency memory c) internal {
+        currencies.push(c);
+        uint256 id = 0;
+        if (c.currencyType == 2) {
+            id = c.ERC1155SmallId;
+            if (c.ERC1155Id != 0) id = c.ERC1155Id;
+        }
         emit CurrencyAdded(
             currencies.length - 1,
-            currencyType,
-            location,
-            currencyId
+            c.currencyType,
+            c.location,
+            id
         );
     }
 }
--- test/CurrencyManager.test.js
+++ test/CurrencyManager.test.js
@@ -1,5 +1,5 @@
 const CurrencyManager = artifacts.require("CurrencyManager");
-
+const BigNumber = require('bignumber.js');
 const { getEvent, getRevert } = require('./utils');
 
 
@@ -10,16 +10,47 @@ contract("CurrencyManager", accounts => {
         instance = await CurrencyManager.new();
     });
 
-    it('allows adding new currencies', async () => {
-        let tx = await instance.addCurrency(0, accounts[0], 0);
+    it('allows adding ERC20 and ERC721 currencies', async () => {
+        let tx = await instance.addERC20Currency(accounts[0]);
         let ev = await getEvent(tx, 'CurrencyAdded');
-        assert.equal(ev.id.toNumber(), 1, "Id is 1, since 0 refers to ether");
-        assert.equal(ev.currencyType.toNumber(), 0);
+        assert.equal(ev.id, 1, 'Id starts at 1 since 0 refers to ether');
+        assert.equal(ev.currencyType, 0);
         assert.equal(ev.location, accounts[0]);
+        assert.equal(ev.ERC1155Id, 0);
+
+        tx = await instance.addERC721Currency(accounts[0]);
+        ev = await getEvent(tx, 'CurrencyAdded');
+        assert.equal(ev.id, 2);
+        assert.equal(ev.currencyType, 1);
+        assert.equal(ev.location, accounts[0]);
+        assert.equal(ev.ERC1155Id, 0);
     });
 
+    it('allows ERC1155 currencies', async () => {
+        tx = await instance.addERC1155TokenCurrency(accounts[0], 1);
+        let ev = await getEvent(tx, 'CurrencyAdded');
+        assert.equal(ev.id, 1);
+        assert.equal(ev.currencyType, 2);
+        assert.equal(ev.location, accounts[0]);
+        assert.equal(ev.ERC1155Id, 1);
+
+        tx = await instance.addERC1155Currency(accounts[0]);
+        ev = await getEvent(tx, 'CurrencyAdded');
+        assert.equal(ev.id, 2);
+        assert.equal(ev.currencyType, 3);
+        assert.equal(ev.location, accounts[0]);
+        assert.equal(ev.ERC1155Id, 0);
+    });
+
+    it('handles large currencyIds efficiently', async () => {
+        let n = BigNumber(2).pow(256).minus(1);
+        let tx = await instance.addERC1155TokenCurrency(accounts[0], n);
+        let ev = await getEvent(tx, 'CurrencyAdded');
+        assert.equal(BigNumber(ev.ERC1155Id).toString(), n.toString());
+    })
+
     it('exposes existing currencies', async () => {
-        await instance.addCurrency(0, accounts[0], 0);
+        await instance.addERC20Currency(accounts[0]);
         let c = await instance.currencies(1);
         assert.equal(c.currencyType.toNumber(), 0);
         assert.equal(c.location, accounts[0]);
