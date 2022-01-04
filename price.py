21f7ce2ff76fc1e6219bde2da7c3f131350c14c8
Mikolaj Figurski <meek.f80@gmail.com>
2022-01-04 15:12:32 -0500

    Add annotations to price calculator script

--- price.py
+++ price.py
@@ -1,10 +1,12 @@
 import sys
 
-def get_price(gas, gasPrice, etherPrice):
-    return int(gas) * float(gasPrice) * .000000001 * float(etherPrice)
 
-if __name__ == '__main__':
+def get_price(gas, gasPrice, etherPrice):
+    return int(gas) * float(gasPrice) * 0.000000001 * float(etherPrice)
+
+
+if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print('Usage: python3 price.py <gas> <gasPrice> <etherPrice>')
+        print("Usage: python3 price.py <gas> <gasPrice (gwei)> <etherPrice (USD)>")
         sys.exit(1)
-    print('$' + str(round(get_price(sys.argv[1], sys.argv[2], sys.argv[3]), 2)))
+    print("$" + str(round(get_price(sys.argv[1], sys.argv[2], sys.argv[3]), 2)))
