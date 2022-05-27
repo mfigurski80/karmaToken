c7aa284a43c07290cbb8f9305d7a21987fb3000e
Mikolaj Figurski <meek.f80@gmail.com>
2022-05-27 16:14:49 -0400

    Add erc1155 collateral tests

--- test/CollateralManager.test.js
+++ test/CollateralManager.test.js
@@ -1,6 +1,7 @@
 const LBondManager = artifacts.require('LBondManager');
 const ERC20Exposed = artifacts.require('ERC20Exposed');
 const ERC721Exposed = artifacts.require('ERC721Exposed');
+const ERC1155Exposed = artifacts.require('ERC1155Exposed');
 const CollateralManager = artifacts.require('CollateralManager');
 
 const { getEvent, getRevert, buildBondBytes, CURRENCY_TYPE } = require('./utils');
@@ -9,6 +10,7 @@ contract('CollateralManager', accounts => {
     let libraryInstance;
     let erc20Instance;
     let erc721Instance;
+    let erc1155Instance;
     let instance;
     const args = { name: 'Test Token', symbol: 'TST', uri: 'http://localhost' };
     const bondBytes = buildBondBytes({
@@ -24,7 +26,8 @@ contract('CollateralManager', accounts => {
         libraryInstance = await LBondManager.new();
         CollateralManager.link(libraryInstance);
         erc20Instance = await ERC20Exposed.new();
-        erc721Instance = await ERC721Exposed.new("name", "symbol", "uri");
+        erc721Instance = await ERC721Exposed.new(args.name, args.symbol, args.uri);
+        erc1155Instance = await ERC1155Exposed.new();
     });
 
     beforeEach(async () => {
@@ -52,9 +55,37 @@ contract('CollateralManager', accounts => {
         await erc721Instance.setApprovalForAll(instance.address, true);
         let currencyEvent = await instance.addERC721Currency(erc721Instance.address).then(tx => getEvent(tx, 'CurrencyAdded'));
         let bondEvent = await instance.mintBond(bondBytes[0], bondBytes[1]).then(tx => getEvent(tx, 'Transfer'));
-        // text subject: associate some erc721 collateral
+        // test subject: associate some erc721 collateral
         let ev = await instance.addERC721Collateral(bondEvent.tokenId, currencyEvent.id, NFT_ID).then(tx => getEvent(tx, 'CollateralAdded'));
         assert.equal(ev.id.toNumber(), bondEvent.tokenId.toNumber());
         assert.equal(ev.collateralType, CURRENCY_TYPE.ERC721);
     });
+
+    it('allows saving erc1155 token collateral', async () => {
+        // setup: mint to erc1155, register it as currency, mint bond
+        const TOKEN_ID = 1;
+        await erc1155Instance.mint(accounts[0], TOKEN_ID, 100);
+        await erc1155Instance.setApprovalForAll(instance.address, true);
+        let currencyEvent = await instance.addERC1155TokenCurrency(erc1155Instance.address, TOKEN_ID).then(tx => getEvent(tx, 'CurrencyAdded'));
+        let bondEvent = await instance.mintBond(bondBytes[0], bondBytes[1]).then(tx => getEvent(tx, 'Transfer'));
+        // test subject: associate some erc1155 collateral
+        let ev = await instance.addERC1155TokenCollateral(bondEvent.tokenId, currencyEvent.id, 10).then(tx => getEvent(tx, 'CollateralAdded'));
+        assert.equal(ev.id.toNumber(), bondEvent.tokenId.toNumber());
+        assert.equal(ev.collateralType, CURRENCY_TYPE.ERC1155Token);
+    });
+
+    it('allows saving erc1155 nft collateral', async () => {
+        // setup: mint to erc1155, register it as currency, mint bond
+        const NFT_ID = 2;
+        await erc1155Instance.mint(accounts[0], NFT_ID, 1);
+        await erc1155Instance.setApprovalForAll(instance.address, true);
+        let currencyEvent = await instance.addERC1155Currency(erc1155Instance.address).then(tx => getEvent(tx, 'CurrencyAdded'));
+        let bondEvent = await instance.mintBond(bondBytes[0], bondBytes[1]).then(tx => getEvent(tx, 'Transfer'));
+        // test subject: associate some erc1155 collateral
+        let ev = await instance.addERC1155NFTCollateral(bondEvent.tokenId, currencyEvent.id, NFT_ID).then(tx => getEvent(tx, 'CollateralAdded'));
+        assert.equal(ev.id.toNumber(), bondEvent.tokenId.toNumber());
+        assert.equal(ev.collateralType, CURRENCY_TYPE.ERC1155NFT);
+    });
+
+
 })
