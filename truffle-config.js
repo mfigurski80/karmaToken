d92eb847a7e73242db4910f8636dd03247dc11ea
Mikolaj Figurski <meek.f80@gmail.com>
2022-06-21 12:19:29 -0400

    Set up local test net

--- truffle-config.js
+++ truffle-config.js
@@ -42,16 +42,16 @@ module.exports = {
     // options below to some value.
     //
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 9545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
-    //  gasPrice: 1,
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 9545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+      //  gasPrice: 1,
     },
-	test: {
-	 host: "10.7.8.120",
-	 port: 9545,
-	 network_id: "*",
-	}
+    test: {
+      host: "10.7.8.120",
+      port: 8545,
+      network_id: "*",
+    }
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
