41b9bc9b7b4ccbe4c15944b4ec0e1ee3c0f32137
Mikolaj Figurski <meek.f80@gmail.com>
2022-07-09 14:59:03 -0400

    Surface user and doc comments for methods

--- public/App.vue
+++ public/App.vue
@@ -1,55 +1,38 @@
 <template>
-    <button v-if="!connected" @click="connect">Connect Wallet</button>
-    <button v-if="connected" @click="callContractMethod">Call Random Method</button>
-    <ContractInteraction :address="address" />
+  <button v-if="!connected" @click="connect">Connect Wallet</button>
+  <div v-if="connected" v-for="c in contractData" :key="c.name">
+    <ContractInteraction :contract="c" :web3="web3"/>
+  </div>
 </template>
 
 <script>
 import ContractInteraction from './ContractInteraction.vue';
 
 export default {
-    name: 'App',
-    components: {
-        ContractInteraction,
+  name: 'App',
+  components: {
+    ContractInteraction,
+  },
+  data: () => ({
+    connected: false,
+    web3: null,
+    contractData: [{
+      name: 'CollateralManager',
+      address: '0xDD5D7Bd43be613c21645E07d64a9e89cf59CaE55',
+      abiPath: '/contracts/CollateralManager.json',
+    }],
+  }),
+  methods: {
+    async connect() {
+        if (!window.ethereum) {
+          alert('Please install metamask or another web3 provider');
+          return;
+        }
+        await window.ethereum.request({method: 'eth_requestAccounts'});
+        this.web3 = new Web3(window.ethereum);
+        this.connected = true;
     },
-    data: () => ({
-        address: "0xDD5D7Bd43be613c21645E07d64a9e89cf59CaE55",
-        abiPath: '/contracts/CollateralManager.json',
-        connected: false,
-        web3: null,
-        contract: null,
-        contractData: [{
-            name: 'CollateralManager',
-            address: '0xDD5D7Bd43be613c21645E07d64a9e89cf59CaE55',
-            abiPath: '/contracts/CollateralManager.json',
-            contract: null,
-        }],
-        /* web3: new Web3('http://localhost:9545'), */
-    }),
-    methods: {
-        async connect() {
-            if (!window.ethereum) {
-                alert('Please install metamask or another web3 provider');
-                return
-            }
-            await window.ethereum.request({method: 'eth_requestAccounts'});
-            const web3 = new Web3(window.ethereum);
-            this.contractData = await Promise.all(this.contractData
-                .map(async c => ({
-                    ...c,
-                    contract: await fetch(c.abiPath)
-                        .then(res => res.json())
-                        .then(cJ => new web3.eth.Contract(cJ.abi, c.address)),
-                })
-            ));
-            console.log(this.contractData);
-            this.web3 = web3;
-            this.connected = true;
-        },
-        async callContractMethod() {
-            
-        },
-    },
+  },
 }
 </script>
 
--- public/ContractInteraction.vue
+++ public/ContractInteraction.vue
@@ -1,14 +1,83 @@
 <template>
-    <p>Contract Interaction</p>
-    <h5>Contract is deployed at: <code>
-        {{ address }}
-    </code></h5>
+  <h3>{{contract.name}} Contract deployed at: 
+    <code>{{ contract.address }}</code>
+  </h3>
+  <h6>Status: {{connected ? 'Connected' : 'Disconnected'}}</h6>
+  <div v-for="f in fields" :key="f.id">
 
+    <form @submit.prevent="performMethod(f.id)">
+      <hr />
+      <h4>Function: <code>{{f.name}}</code></h4>
+      <p>{{f.notice}}</p>
+      <p>{{f.details}}</p>
+      <label v-if="f.payable">Wei to send: <input placeholder="10000"
+        v-model.number="f.payableValue"
+      /></label>
+      <div v-for="i in f.inputs">
+
+        <label> Parameter: {{i.name}} {{i.type}} -- {{i.description}} <input
+          v-model='i.value'
+          :placeholder="placeholders[i.type]"
+        /></label> 
+
+      </div>
+      <button type="submit">Call Method</button>
+    </form>
+
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'ContractInteraction',
-    props: ["address"],
+  name: 'ContractInteraction',
+  props: ["contract", "web3"],
+  data: () => ({
+    connected: false,
+    obj: null,
+    fields: [],
+    placeholders: {
+      'uint256': '100',
+      'uint256[]': '[100, 101]',
+      'address': '0xDD5D7Bd43be613c21645E07d64a9e89cf59CaE55',
+      'address[]': '[0xDD5D7Bd43be613c21645E07d64a9e89cf59CaE55]',
+      'bytes32': '0x0000000000000000000000000000000000000000000000000000000000000000',
+      'bytes4': '0x00000000',
+      'bool': 'true',
+    },
+  }),
+  async mounted() {
+    if (!this.web3) alert('ERR: interaction without web3 connection!');
+    const cJ = await fetch(this.contract.abiPath)
+      .then(res => res.json());
+    this.obj = new this.web3.eth.Contract(cJ.abi, this.contract.address);
+    this.connected = true;
+    this.fields = this.parseMethods(cJ);
+  },
+  methods: {
+    parseMethods(cJ) {
+      const userDoc = Object.fromEntries(Object.keys(cJ.userdoc.methods)
+        .map(fnSig => [fnSig.split('(')[0], cJ.userdoc.methods[fnSig].notice]
+      ));
+      const devDoc = Object.fromEntries(Object.keys(cJ.devdoc.methods)
+        .map(fnSig => [fnSig.split('(')[0], cJ.devdoc.methods[fnSig]]
+      ));
+      return cJ.abi.filter(el => el.type === 'function')
+        .map((fn,i) => ({
+          ...fn, 
+          id: i,
+          notice: userDoc[fn.name],
+          details: devDoc[fn.name]?.details,
+          inputs: fn.inputs.map(inp => ({
+            ...inp,
+            description: devDoc[fn.name]?.params?.[inp.name],
+          })),
+        }));
+    },
+    async performMethod(id) {
+      // user submitted data for specific method to be performed
+      console.log(id);
+      console.log(this.fields[id]);
+    },
+  },
 }
 </script>
